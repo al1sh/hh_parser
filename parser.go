5a2df4b48e72bf4e92e2c3e694a70ca2efb3a9fa
Alish <firstrestrest@gmail.com>
2018-06-09 02:09:44 +0300

    Basic sql queries completed

--- parser.go
+++ parser.go
@@ -8,24 +8,21 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Vacancy is a type with the fields used for parsing
 type Vacancy struct {
-	ID         int
-	Position   string
-	Company    string
-	Link       string
-	City       string
-	Experience string
-	Language   string
-	Date       string
-	Details    string
+	Position string
+	Company  string
+	Link     string
+	City     string
+	Details  string
 }
 
 // PrepareDB and return it
 func PrepareDB() *sql.DB {
-	database, err := sql.Open("sqlite3", "./vacancies.db")
+	database, err := sql.Open("sqlite3", "vacancies.sqlite")
 	if err != nil {
 		panic(err)
 	}
@@ -37,15 +34,12 @@ func PrepareDB() *sql.DB {
 func CreateTable(db *sql.DB) {
 	sqlTable := `
 	CREATE TABLE IF NOT EXISTS vacancies(
-		Id TEXT NOT NULL ,
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
 		Position TEXT,
 		Company TEXT,
 		Link TEXT,
 		City TEXT,
-		Experience TEXT,
-		Language: TEXT,
-		Date: TEXT
-		DETAILS: TEXT
+		Details TEXT
 	);
 	`
 
@@ -58,16 +52,7 @@ func CreateTable(db *sql.DB) {
 // InsertVacancy stores found vacancis in the db
 func InsertVacancy(db *sql.DB, jobs []Vacancy) {
 	sqlAdd := `
-	INSERT OR REPLACE INTO vacancies(
-		Position 
-		Company 
-		Link 
-		City 
-		Experience 
-		Language 
-		Date
-		DETAILS
-	) values(?, ?, ?, ?, ?, ? ,? ,?)
+	INSERT OR REPLACE INTO vacancies(Position, Company, Link, City, Details) VALUES (?, ?, ?, ?, ?);
 	`
 
 	stmt, err := db.Prepare(sqlAdd)
@@ -77,7 +62,8 @@ func InsertVacancy(db *sql.DB, jobs []Vacancy) {
 	defer stmt.Close()
 
 	for _, job := range jobs {
-		_, err2 := stmt.Exec(job.Position, job.Company, job.Link, job.City, job.Experience, job.Language, job.Date, job.Details)
+		fmt.Println(job.Position)
+		_, err2 := stmt.Exec(job.Position, job.Company, job.Link, job.City, job.Details)
 		if err2 != nil {
 			panic(err2)
 		}
@@ -85,18 +71,23 @@ func InsertVacancy(db *sql.DB, jobs []Vacancy) {
 }
 
 // ExistsVacancy checks if given job exists in the database
-func ExistsVacancy(db *sql.DB) bool {
-	sqlRead := `
-	SELECT Position, Company, City, Experience, Language FROM vacancies
-	`
+func ExistsVacancy(db *sql.DB, job Vacancy) bool {
+	sqlRead := fmt.Sprintf(`SELECT ID FROM vacancies WHERE Position="%s" AND Company="%s" AND City="%s";`,
+		job.Position, job.Company, job.City)
 
+	fmt.Println(sqlRead)
 	rows, err := db.Query(sqlRead)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
-	if rows != nil {
+	// var id int
+	if rows.Next() {
+		// rows.Scan(&id)
+		// if id != 0 {
+		// 	fmt.Println(id)
+		// }
 		return true
 	}
 	return false
@@ -113,7 +104,7 @@ func ExistsVacancy(db *sql.DB) bool {
 }
 
 // ExampleScrape scrapes given URL
-func ExampleScrape() {
+func ExampleScrape() []Vacancy {
 	// Request the HTML page.
 	res, err := http.Get("https://hh.ru/search/vacancy?text=%28junior+OR+trainee+OR+intern%29+and+%28" +
 		"Go+OR+Golang+OR+Python%29&only_with_salary=false&order_by=publication_time&specialization=1" +
@@ -132,11 +123,15 @@ func ExampleScrape() {
 		log.Fatal(err)
 	}
 
+	jobs := []Vacancy{}
+
 	// Find the review items
 	doc.Find(".vacancy-serp-item").Each(func(i int, s *goquery.Selection) {
 		address := s.Find("span.vacancy-serp-item__meta-info").First()
 		city := strings.Split(address.Text(), ",")[0]
-		fmt.Println(city)
+		// fmt.Println(city)
+
+		position, link, employer := "", "", ""
 
 		links := s.Find("a")
 		for j := range links.Nodes {
@@ -144,23 +139,36 @@ func ExampleScrape() {
 			data, _ := single.Attr("data-qa")
 
 			if data == "vacancy-serp__vacancy-title" {
-				position := single.Text()
-				link, _ := single.Attr("href")
-				fmt.Println(position, link)
+				position = single.Text()
+				link, _ = single.Attr("href")
+				// fmt.Println(position, link)
 			}
 
 			if data == "vacancy-serp__vacancy-employer" {
-				employer := single.Text()
-				fmt.Println(employer)
+				employer = single.Text()
+				// fmt.Println(employer)
 			}
 		}
-		fmt.Println()
+
+		details := "DESCRIPTION GOES HERE"
+
+		job := Vacancy{position, employer, link, city, details}
+		jobs = append(jobs, job)
+		// fmt.Println()
 
 	})
+
+	return jobs
 }
 
 func main() {
+	db := PrepareDB()
+	CreateTable(db)
+	allJobs := ExampleScrape()
+	InsertVacancy(db, allJobs)
+	isExists := ExistsVacancy(db, allJobs[0])
+	fmt.Println(isExists)
 
-	ExampleScrape()
+	// fmt.Println(allJobs)
 	fmt.Println("Finished")
 }
